e95e9c9355ad299b264d43283ea1d9af2c0c66ed
deokjin <ghaehfdl7@naver.com>
2025-12-10 19:37:55 +0900

    feat: className props 추가

--- components/_common/profile.tsx
+++ components/_common/profile.tsx
@@ -1,9 +1,14 @@
 import { User } from "lucide-react";
+import { cn } from "@/lib/utils/tailwindHelper";
 
-export default function Profile() {
+type ProfileProps = {
+  className?: string;
+};
+
+export default function Profile({ className }: ProfileProps) {
   return (
-    <div className="bg-neutral-90 text-neutral-60 rounded-full p-2">
-      <User />
+    <div className={"bg-neutral-90 text-neutral-60 rounded-full p-2"}>
+      <User className={className} />
     </div>
   );
 }
