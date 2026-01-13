a3b9d48e34bd840b703926e88e3c6a2167d7ca40
deokjin <ghaehfdl7@naver.com>
2026-01-13 18:25:14 +0900

    refactor: page 서버 컴포넌트화 준비

--- app/mypage/recent/page.tsx
+++ app/mypage/recent/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import RecentPostList from "@/components/mypage/recent/recentPostList";
+import RecentSection from "@/components/mypage/recent/recentSection";
 
 /**
  * 최근에 본 글 페이지
@@ -9,15 +7,5 @@ import RecentPostList from "@/components/mypage/recent/recentPostList";
  * - 8개씩 한 페이지로 표시 (페이지네이션)
  */
 export default function RecentPostsPage() {
-  return (
-    <div className="border-surface-98 flex flex-col gap-[30px] rounded-xl border bg-white p-10">
-      {/* 페이지 제목 */}
-      <h1 className="text-neutral-5 text-xl leading-[1.2] font-bold tracking-[-0.01em]">
-        최근에 본 글
-      </h1>
-
-      {/* 게시글 리스트 */}
-      <RecentPostList />
-    </div>
-  );
+  return <RecentSection />;
 }
