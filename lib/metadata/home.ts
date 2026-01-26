a64dfcd8ec3beecab40ae8460375bdc23ea379e5
deokjin <ghaehfdl7@naver.com>
2026-01-26 21:40:09 +0900

    fix: 홈페이지 메타데이터 수정

--- lib/metadata/home.ts
+++ lib/metadata/home.ts
@@ -1,13 +1,12 @@
 import { Metadata } from "next";
-import { createMetadata } from "./helpers";
+import { createMetadata, SITE_CONFIG } from "./helpers";
 
 /**
  * 홈페이지 메타데이터
  */
 export const homeMetadata: Metadata = createMetadata({
-  title: "모두의식물 - 우리들의 식물 이야기 | ModusPlant",
-  description:
-    "식물 애호가들의 커뮤니티, 모두의식물에서 식물 키우기 노하우, 분갈이 팁, 병충해 관리 등 다양한 식물 이야기를 공유하세요. 반려식물과 함께하는 특별한 일상을 시작하세요.",
+  title: SITE_CONFIG.name,
+  description: SITE_CONFIG.description,
   path: "/",
   keywords: [
     "식물 키우기",
