e8b4b48420f112effda2cae6c20894543c03d6f1
deokjin <ghaehfdl7@naver.com>
2026-02-10 03:24:01 +0900

    style: 모달 배경 추가 및 디자인 수정

--- components/_common/modal/snackbarModal.tsx
+++ components/_common/modal/snackbarModal.tsx
@@ -14,7 +14,7 @@ export default function SnackbarModal({
       className={cn(
         "fixed bottom-10 left-1/2 z-99 -translate-x-1/2 transform",
         "rounded-[15px] bg-neutral-100 px-16 py-7 shadow-lg lg:h-17",
-        "text-neutral-10 lg:text-size-[16px] min-w-80 text-center font-medium",
+        "text-neutral-10 min-w-80 text-center font-medium lg:text-[16px]",
         "transition-all duration-300",
         isAnimating ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
       )}
