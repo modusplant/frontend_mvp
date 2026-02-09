ba45f75c7f0eb1b1fd81f9aebbc89c0a0143b7f0
deokjin <ghaehfdl7@naver.com>
2026-02-10 01:05:31 +0900

    style: 스타일 값 수정

--- components/mypage/common/postListItemMetadata.tsx
+++ components/mypage/common/postListItemMetadata.tsx
@@ -48,7 +48,7 @@ export default function PostListItemMetadata({
         <Bookmark
           fill={post.isBookmarked ? "currentColor" : "none"}
           className={cn(
-            "h-[15px] w-[15px] md:h-[17px] md:w-[17px]",
+            "h-3.75 w-3.75 md:h-4.25 md:w-4.25",
             post.isBookmarked ? "text-primary-50" : "text-neutral-60"
           )}
         />
